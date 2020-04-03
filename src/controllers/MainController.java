51e2fe141f7f33b3d62a398cf7366f57f5510a42
dewangga-pk <pacetcommunity570@gmail.com>
2020-04-03 07:58:02 +0700

    Local to Github

---
+++ src/controllers/MainController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class MainController {
+
+    @FXML
+    private Text berasText;
+
+    @FXML
+    private Text kedelaiText;
+
+    @FXML
+    private Text jagungText;
+
+    @FXML
+    private Text gandumText;
+}
