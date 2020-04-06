a0e287970f5badcd617f01ee91736f6b8a9fa492
dewangga-pk <pacetcommunity570@gmail.com>
2020-04-06 20:42:51 +0700

    Tombol Reset

--- src/controllers/RegisterController.java
+++ src/controllers/RegisterController.java
@@ -1,15 +1,51 @@
 package controllers;
 
 
+import com.jfoenix.controls.JFXCheckBox;
 import helpers.Helper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class RegisterController {
+    @FXML
+    private TextField noIndukText;
+
+    @FXML
+    private TextField emailText;
+
+    @FXML
+    private TextField userNameText;
+
+    @FXML
+    private PasswordField passwordText;
+
+    @FXML
+    private PasswordField passwordReText;
+
+    @FXML
+    private TextField namaLengkapText;
+
+    @FXML
+    private TextField noTelpText;
+
+    @FXML
+    private JFXCheckBox checkListRegister;
 
     @FXML
     void loginBtn(ActionEvent event) {
         Helper.changePage(event,"Main");
     }
-
+    @FXML
+    void resetBtn(ActionEvent event) {
+        noIndukText.setText("");
+        emailText.setText("");
+        userNameText.setText("");
+        passwordText.setText("");
+        passwordReText.setText("");
+        namaLengkapText.setText("");
+        noTelpText.setText("");
+        checkListRegister.setSelected(false);
+    }
 }
